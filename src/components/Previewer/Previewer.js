d1c362ad3085b4381648ccca225788adb22c41be
Funsis <s.korovin95@gmail.com>
2018-01-26 16:09:36 +0300

    Add state to Previewer component

--- src/components/Previewer/Previewer.js
+++ src/components/Previewer/Previewer.js
@@ -2,19 +2,27 @@ import React, { Component } from "react";
 import { Row, Col, FormGroup, FormControl } from "react-bootstrap";
 
 class Previewer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { text: "" };
+  }
+
+  onChangeHandler = event => {
+    this.setState({ text: event.target.value });
+  };
+
   render() {
     return (
       <Row>
         <Col sm={6}>
           <form>
             <FormGroup>
-              <FormControl componentClass="textarea" />
+              <FormControl componentClass="textarea" value={this.state.text} onChange={this.onChangeHandler} />
             </FormGroup>
           </form>
         </Col>
-        <Col sm={6}>
-          previewed text
-        </Col>
+        <Col sm={6}>previewed text</Col>
       </Row>
     );
   }
